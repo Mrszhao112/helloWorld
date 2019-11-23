f0694937b2bacd9675915ef40d5b62f9b22117d3
summer <56674834+Mrszhao112@users.noreply.github.com>
2019-11-23 18:58:54 +0800

    Update string.h

--- string.h
+++ string.h
@@ -103,4 +103,113 @@ int myStrcmp(const char* str1,const char* str2)
 	}
 	return(ret);
 }
+6.实现memcpy
+函数memcpy从source的位置开始
+向后复制num个字节的数据到destination的内存位置。
+这个函数在遇到 ‘\0’ 的时候并不会停下来。
+如果source和destination有任何的重叠，复制的结果都是未定义的
+void* myMemcpy(void* dst,const void* src,size_t num)
+{
+	assert(dst&&src);
+	char* strSrc = (char*)src;
+	char* strDst = (char*)dst;
+	while (num)
+	{
+		*strDst= *strSrc;
+		
+		strDst++;
+		strSrc++;
+		num--;
+	}
+	return dst;
+}
+7.实现memmove
+和memcpy的差别就是memmove函数处理的源内存块和目标内存块是可以重叠的。
+如果源空间和目标空间出现重叠，就得使用memmove函数处理
+`void* myMemove(void* dst,const void* src,size_t num)
+{
+	assert(dst&&src);
+	char* strDst = (char*)dst;
+	char* strSrc = (char*)src;
+	//分为前重叠和后重叠
+	if (strDst<strSrc&&(strDst+num)>strSrc)
+	{
+		while (num)
+		{
+			*strDst = *strSrc;
+			num--;
+			strSrc++;
+			strDst++;
+		}
+	} 
+	else if(strSrc<strDst&&strSrc+num>strDst)
+	{
+		while (num)
+		{
+			*(strDst + num) = *(strSrc + num);
+			num--;
+		}
+	}
+	else
+	{
+		while (num)
+		{
+			*strDst = *strSrc;
+			num--;
+			strSrc++;
+			strDst++;
+		}
+	}
+	return dst;
+}
+8.实现strncpy
+Copies the ﬁrst num characters of source to destination. If the end of the source C string (which is signaled by a null-character) is found before num characters have been copied, destination is padded with zeros until a total of num characters have been written to it.
+拷贝num个字符从源字符串到目标空间。
+如果源字符串的长度小于num，
+则拷贝完源字符串之后，在目标的后边追加0，直到num个
+char* myStrncpy(char* dest, const char* src, size_t num)
+{
+	assert(dest&&src);
+	char* ret = dest;
+	num--;
+	while ((*dest++ = *src++) && (num--))
+	{
+		;
+	}
+	return ret;
+}
 
+9.实现strncat
+Appends the ﬁrst num characters of source to destination, plus a terminating null-character. If the length of the C string in source is less than num, only the content up to the terminating nullcharacter is copied.
+char* myStrncat(char*dest, const char*src, size_t num)
+{
+	assert(dest&&src);
+	char* ret = dest;
+	while (*++dest);
+
+	while (src&&num)
+	{
+		*dest++ = *src++;
+		num--;
+	}
+	return ret;
+}
+10.实现strncmp
+
+比较到出现另个字符不一样或者一个字符串结束或者num个字符全部比较完
+int myStrncmp(const char* dest, const char* src, size_t num)
+{
+	assert(dest&&src);
+	while (dest++&&src++&&num--)
+	{
+		if (*dest > *src)
+		{
+			return 1;
+		}
+		if (*dest < *src)
+		{
+			return -1;
+		}
+	}
+	return 0;
+}
